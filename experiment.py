a839dd593720de8a6598164be42d45c4dc9e1f19
BabyBeo2k1 <hung.lq191881@sis.hust.edu.vn>
2023-10-19 11:59:59 +0700

    update quickstart

--- experiment.py
+++ experiment.py
@@ -1,10 +1,17 @@
 from method import crawlMethod
+import json
+
 def main():
     time=1609434000 #data will crawl/created after creation of chat GPT (algorithm is sound but not complete)
     out='./data.jsonl'#output will have jsonl file
-    reponame="keel"
-    username ="Harium"
-    crawler=crawlMethod(reponame,username)
-    crawler.crawl(time,out)
+    
+    repos=[]
+    with open("./repos_list.txt","r") as file:
+        for line in file.readlines():
+            repos.append(line.strip())
+    for repo in repos:
+        username,reponame=repo.split("/")
+        crawler=crawlMethod(reponame,username)
+        crawler.crawl(time=time,out=out)
 if __name__=="__main__":
     main()
